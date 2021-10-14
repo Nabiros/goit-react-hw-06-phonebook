a485fa95d7753fb894a41c80e63deeba3ac30ec8
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-10-14 15:43:07 +0300

    set redux & create components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./components/App/App";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+    ,
   </React.StrictMode>,
   document.getElementById("root")
 );
